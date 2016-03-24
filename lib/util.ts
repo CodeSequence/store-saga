bd493c3ca164c2960c6e727a7568c59a2052f0c9
Mike Ryan <mike.ryan52@gmail.com>
2016-03-24 09:22:28 -0600

    feat(applySelector): Add utility to apply a selector fn to an iteration's state

--- lib/util.ts
+++ lib/util.ts
@@ -1,59 +1,72 @@
 import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
-import {Provider, provide, OpaqueToken, Injector} from 'angular2/core';
-import {POST_MIDDLEWARE, Dispatcher, Action, Middleware} from '@ngrx/store';
-import {Observable} from 'rxjs/Observable';
 
-import {SagaRunner} from './runner';
-import {SagaIteration, Saga, SagaFactory} from './interfaces';
+import { Provider, provide, OpaqueToken, Injector } from 'angular2/core';
+import { POST_MIDDLEWARE, Dispatcher, Action, usePostMiddleware } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { SagaRunner, INIT_SAGAS } from './runner';
+import { SagaIteration, Saga, SagaFactory } from './interfaces';
+import { sagaMiddleware } from './middleware';
 
 export function createSaga<T>(factory: SagaFactory<T>, deps: any[] = []){
+  if(factory.length !== deps.length) {
+    throw new Error(`Cannot resolve all parameters for saga factory`);
+  }
+
   return provide(new OpaqueToken('@ngrx/store Saga Effect'), {
     deps,
     useFactory: factory
   });
 }
 
+export function addInitSagas(...sagas: Provider[]): Provider[] {
+  return sagas.map(saga => provide(INIT_SAGAS, {
+    multi: true,
+    useValue: saga
+  }));
+}
+
 export function installSagaMiddleware(...sagas: Provider[]): Provider[] {
-  function factory(dispatcher: Dispatcher<any>, runner: SagaRunner): Middleware {
-    sagas.forEach(saga => runner.run(saga));
-
-    return state$ => state$
-      .withLatestFrom(dispatcher)
-      .map(([ state, action ]) => {
-        runner.next({ state, action });
-
-        return state;
-      });
-  }
-
   return [
-    provide(POST_MIDDLEWARE, {
-      multi: true,
-      deps: [ Dispatcher, SagaRunner ],
-      useFactory: factory
-    }),
-    provide(SagaRunner, {
-      useClass: SagaRunner
-    })
+    provide(SagaRunner, { useClass: SagaRunner }),
+    ...usePostMiddleware(sagaMiddleware),
+    ...addInitSagas(...sagas)
   ];
 }
 
 
 export function whenAction(...types: string[]) {
-  return function(t: SagaIteration<any>): boolean {
-    return types.indexOf(t.action.type) >= 0;
+  return function(iteration: SagaIteration<any>): boolean {
+    return types.indexOf(iteration.action.type) >= 0;
   }
 }
 
-export function toPayload(t: SagaIteration<any>): any {
-  return t.action.payload;
-}
-
-export function put(action: Action): Observable<Action> {
-  return Observable.of(action);
+export function toPayload(iteration: SagaIteration<any>): any {
+  return iteration.action.payload;
 }
 
 export function all() {
   return false;
+}
+
+
+export function applySelector<T,V>(selectorFn: (obs$: Observable<T>) => Observable<V>) {
+  return function(saga$: Observable<SagaIteration<T>>): Observable<SagaIteration<V>> {
+    const selected$ = saga$.map(s => s.state).let(selectorFn);
+
+    return saga$
+      .withLatestFrom(selected$)
+      .map(([ iteration, selected ]) => {
+        return {
+          action: iteration.action,
+          state: selected
+        };
+      });
+  }
+}
+
+export function put(action: Action): Observable<Action> {
+  return Observable.of(action);
 }
