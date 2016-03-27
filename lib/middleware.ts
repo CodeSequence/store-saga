920edfa224a2d7799101c9df67869a58b4dda259
Mike Ryan <mike.ryan52@gmail.com>
2016-03-27 09:34:20 -0600

    refactor(middleware): Refactor middleware for clarity

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -1,27 +1,16 @@
 import 'rxjs/add/operator/withLatestFrom';
 import 'rxjs/add/operator/map';
-import { Injector, Provider } from 'angular2/core';
+import 'rxjs/add/operator/do';
+import { Provider } from 'angular2/core';
 import { createMiddleware, Dispatcher } from '@ngrx/store';
 
 import { SagaRunner } from './runner';
 
-export function middlewareFactory(dispatcher$: Dispatcher<any>, injector: Injector) {
-  let runner: SagaRunner;
+export const sagaMiddleware = createMiddleware(function middlewareFactory(
+  dispatcher$: Dispatcher<any>, runner: SagaRunner) {
 
   return state$ => state$
-    .withLatestFrom(dispatcher$)
-    .map(([ state, action ]) => {
-      if( !runner ) {
-        runner = injector.get(SagaRunner);
-      }
-
-      runner.next({ state, action });
-
-      return state;
-    });
-}
-
-export const sagaMiddleware = createMiddleware(middlewareFactory, [
-  Dispatcher,
-  Injector
-]);
+    .withLatestFrom(dispatcher$, (state, action) => ({ state, action }))
+    .do(runner)
+    .map(({ state }) => state);
+}, [ Dispatcher, SagaRunner ]);
